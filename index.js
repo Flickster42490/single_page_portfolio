01f83ae49101497703c75ed1fe33189199583348
Max Li <>
2018-11-08 14:55:36 -0600

    update with different pages and resumes. (Kellogg initial update)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+var path = require('path');
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -7,6 +9,15 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
+
+app.get('/tech',function(request, response) {
+  response.sendFile(path.join(__dirname + '/public/tech.html'))
+})
+
+app.get('/thought_concepts',function(request, response) {
+  response.sendFile(path.join(__dirname + '/public/thought_concepts.html'))
+})
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
